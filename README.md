44f30dde138938b08dfffe80a0867594a15a0308
Vasili Slapik <vslapik@gmail.com>
2019-11-22 16:12:59 +0300

    Add Gospers glider gun

--- README.md
+++ README.md
@@ -10,4 +10,8 @@ Have libncurses5-dev installed on your linux box to be able to build it.
 
 **c** - to clear the board
 
-**g** - to spawn a glider from the top left corner into the board (carefull, it is addictive)
+**g** - to spawn a glider from the top left corner on the board (carefull, it is addictive)
+
+**s** - to put the Gosper glider gun at the top left corner on the board
+
+**l** - to flip board loop mode
--- life.c
+++ life.c
@@ -5,11 +5,10 @@
 #include <string.h>
 #include <unistd.h>
 
-//#define LOOP
 #define ARRAY_LEN(a) (sizeof(a) / sizeof(a[0]))
 
 static sig_atomic_t should_exit = false;
-static bool board_loop = false;
+static bool board_loop = true;
 
 static void step(int xmax, int ymax, char board[][xmax])
 {
@@ -54,11 +53,40 @@ static void put_glider(int x, int y, int xmax, int ymax, char board[][ymax])
     assert(x + 2 < xmax);
     assert(y + 2 < ymax);
 
-    board[y + 2][x + 0] = 1;
-    board[y + 2][x + 1] = 1;
-    board[y + 2][x + 2] = 1;
-    board[y + 1][x + 2] = 1;
-    board[y + 0][x + 1] = 1;
+    board[x + 2][y + 0] = 1;
+    board[x + 0][y + 1] = 1;
+    board[x + 2][y + 1] = 1;
+    board[x + 1][y + 2] = 1;
+    board[x + 2][y + 2] = 1;
+}
+
+static void put_gosper_gun(int x, int y, int xmax, int ymax, char board[][ymax])
+{
+    assert(x + 37 < xmax);
+    assert(y + 10 < ymax);
+
+    static const char gg[][2] = {
+        {5, 1},  {6, 1},
+        {5, 2},  {6, 2},
+        {5, 11}, {6, 11}, {7, 11},
+        {4, 12}, {8, 12},
+        {3, 13}, {9, 13},
+        {3, 14}, {9, 14},
+        {6, 15},
+        {4, 16}, {8, 16},
+        {5, 17}, {6, 17}, {7, 17},
+        {6, 18},
+        {3, 21}, {4, 21}, {5, 21},
+        {3, 22}, {4, 22}, {5, 22},
+        {2, 23}, {6, 23},
+        {1, 25}, {2, 25}, {6, 25}, {7, 25},
+        {3, 35}, {4, 35},
+        {3, 36}, {4, 36},
+    };
+
+    for (int i = 0; i < ARRAY_LEN(gg); i++) {
+        board[x + gg[i][0]][y + gg[i][1]] = 1;
+    }
 }
 
 static void fill_random(int xmax, int ymax, char board[][ymax])
@@ -92,16 +120,19 @@ int main(void)
     fill_random(LINES, COLS, board);
 
     while (1) {
-        step(COLS, LINES, board);
+
         refresh();
+
         for (int i = 0; i < LINES; i++) {
             for (int j = 0; j < COLS; j++) {
+                mvaddch(i, j, board[i][j] ? 'o' : ' ');
                 if (should_exit) {
                     goto exit;
                 }
-                mvaddch(i, j, board[i][j] ? 'o' : ' ');
             }
         }
+
+        step(COLS, LINES, board);
 
         long long d = 0;
         do {
@@ -120,6 +151,9 @@ int main(void)
                     case 'g':
                         put_glider(0, 0, LINES, COLS, board);
                         break;
+                    case 's':
+                        put_gosper_gun(0, 0, LINES, COLS, board);
+                        break;
                     case 'c':
                         memset(board, 0, sizeof(board));
                         break;
@@ -131,6 +165,7 @@ int main(void)
                         delay += 1;
                         break;
                 }
+                d = delay + 1;
             }
             usleep(delay > 10000 ? 10000 : delay);
             d += 10000;
